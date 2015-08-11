b85ab9dc55597836ce3a4d2daae96817569c9f26
David <pi@raspberrypi.(none)>
2015-08-11 05:05:56 +0000

    drs

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+from BaseHTTPServer import HTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+ROUTES = [
+    ('/', '/var/www/doc-html')
+]
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def translate_path(self, path):
+        print "path: " + path
+	# default root -> cwd        
+        root = os.getcwd()
+        
+        # look up routes and get root directory
+        for patt, rootDir in ROUTES:
+            if path.startswith(patt):                
+                path = path[len(patt):]
+                root = rootDir
+                break
+        # new path
+        return os.path.join(root, path)    
+
+if __name__ == '__main__':
+    print "Start server"
+    httpd = HTTPServer(('127.0.0.1', 8000), MyHandler)
+    httpd.serve_forever()
+    print "Started server"

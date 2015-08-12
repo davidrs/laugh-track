d3cfb2d8b19fc2b762099aa0895b530cb24b2b09
drustsmith <drustsmith@drustsmith-macbookpro.roam.corp.google.com>
2015-08-11 18:00:50 -0700

    Setup actions based on paths

--- main.py
+++ main.py
@@ -3,27 +3,36 @@ import os
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-ROUTES = [
-    ('/', '/var/www/doc-html')
-]
 
 class MyHandler(SimpleHTTPRequestHandler):
     def translate_path(self, path):
         print "path: " + path
-	# default root -> cwd        
-        root = os.getcwd()
-        
-        # look up routes and get root directory
-        for patt, rootDir in ROUTES:
-            if path.startswith(patt):                
-                path = path[len(patt):]
-                root = rootDir
-                break
-        # new path
-        return os.path.join(root, path)    
 
+        if path == "/laugh":
+            self.play_laugh()
+
+        elif path == "/todo":
+            print "TODO: any action we want."
+
+        else:
+            print "Unkown command: " + path
+
+
+	    # default root -> cwd        
+        root = os.getcwd()
+        # return unchanged path to get any appropriate files.
+        return os.path.join(root, path)
+
+    # Plays a laugh track.     
+    def play_laugh(self):
+        print "TODO: play laugh audio"
+
+
+
+
+# This code will only run if this file is run as the main python file.
 if __name__ == '__main__':
     print "Start server"
     httpd = HTTPServer(('0.0.0.0', 8000), MyHandler)
     httpd.serve_forever()
-    print "Started server"
+    print "Code here and below won't run."
